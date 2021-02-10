e562e9ed1482d9990b979a9af2a21a7254522daa
mzw86020424 <mzw86020424@gmail.com>
2021-02-10 09:50:51 +0900

    Create MyOmikuji app

---
+++ js/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+{
+  const btn = document.getElementById('btn');
+
+  btn.addEventListener('click',()=>{
+    const results = ['大吉','中吉','小吉','末吉'];
+    // btn.textContent = results[Math.floor(Math.random() * results.length)];
+    const n = Math.random();
+
+    if (n < 0.05) {
+      btn.textContent = results[0];
+    } else if (n < 0.2) {
+      btn.textContent = results[1];
+    } else {
+      btn.textContent = results[3];
+    }
+
+  });
+}
